d1eb992b872415715b578be2628abcbf14f2e6e6
riskers <gaoyibobobo@gmail.com>
2018-04-10 15:27:54 +0800

    clean store

--- src/pages/Home/store.js
+++ src/pages/Home/store.js
@@ -3,53 +3,58 @@ import Vuex from 'vuex'
 // simulate newwork deley
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-const userStore = {
-  state: {
-    loading: false,
-    total: 0,
-    data: [],
-    error: ''
+const state = {
+  loading: false,
+  total: 0,
+  data: [],
+  error: '',
+}
+
+const mutations = {
+  FETCH_GITHUB_USERS_LOADING(state) {
+    state.loading = true
   },
-  mutations: {
-    FETCH_GITHUB_USERS_LOADING(state) {
-      state.loading = true
-    },
-    FETCH_GITHUB_SEARCH_USER_FAILURE(state, payload) {
-      state.loading = false
-      state.error = payload.error
-    },
-    FETCH_GITHUB_SEARCH_USER_SUCCESS(state, payload) {
-      state.loading = false,
-      state.total = payload.total_count,
-      state.data = payload.items
-    }
+  FETCH_GITHUB_SEARCH_USER_FAILURE(state, payload) {
+    state.loading = false
+    state.error = payload.error
   },
-  actions: {
-    async searchUser({dispatch, commit}, {username, page}) {
+  FETCH_GITHUB_SEARCH_USER_SUCCESS(state, payload) {
+    state.loading = false,
+    state.total = payload.total_count,
+    state.data = payload.items
+  },
+}
+
+const actions = {
+  async searchUser({dispatch, commit}, {username, page}) {
+    commit({
+      type: 'FETCH_GITHUB_USERS_LOADING'
+    })
+
+    await delay(2000)
+
+    try {
+      const response = await fetch(`https://api.github.com/search/users?q=${username}&page=${page}`)
+      let resData = await response.json()
 
       commit({
-        type: 'FETCH_GITHUB_USERS_LOADING'
+        type: 'FETCH_GITHUB_SEARCH_USER_SUCCESS',
+        total_count: resData.total_count,
+        items: resData.items,
       })
-
-      await delay(2000)
-
-      try {
-        const response = await fetch(`https://api.github.com/search/users?q=${username}&page=${page}`)
-        let resData = await response.json()
-
-        commit({
-          type: 'FETCH_GITHUB_SEARCH_USER_SUCCESS',
-          total_count: resData.total_count,
-          items: resData.items,
-        })
-      } catch(e) {
-        commit({
-          type: 'FETCH_GITHUB_SEARCH_USER_FAILURE',
-          error: 'No This User',
-        })
-      }
+    } catch(e) {
+      commit({
+        type: 'FETCH_GITHUB_SEARCH_USER_FAILURE',
+        error: 'No This User',
+      })
     }
-  }
+  },
+}
+
+const userStore = {
+  state,
+  mutations,
+  actions,
 }
 
 export default userStore
